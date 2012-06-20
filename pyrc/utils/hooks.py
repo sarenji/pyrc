99acf8cdbb9a493eda47560fa6e2de398a0516ed
David Peter <david.a.peter@gmail.com>
2012-06-19 21:57:25 -0400

    Wrap wrapped hooks in functools.wrap

--- pyrc/utils/hooks.py
+++ pyrc/utils/hooks.py
@@ -1,8 +1,11 @@
+import functools
+
 def command(func, matcher = None):
   # Default the command's name to the function's name.
   if matcher is None:
     matcher = func.func_name
 
+  @functools.wraps(func)
   def wrapped_command(*args, **kwargs):
     return func(*args, **kwargs)
   wrapped_command._type = "COMMAND"
@@ -12,6 +15,7 @@ def command(func, matcher = None):
 
 def repeat(interval):
   def wrapped(func):
+    @functools.wraps(func)
     def wrapped_command(*args, **kwargs):
       return func(*args, **kwargs)
     wrapped_command._type = "REPEAT"
