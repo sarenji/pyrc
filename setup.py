9a710617bde74f0c9b691f85950e381e49f35881
David Peter <dpeter@kickstarter.com>
2014-12-13 10:44:46 -0500

    v0.6.7

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='pyrc',
-      version='0.6.6',
+      version='0.6.7',
       description='Simple, clean Python IRC library',
       author='David Peter',
       author_email='david.a.peter@gmail.com',
