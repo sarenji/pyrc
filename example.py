5a454b9cadacb132abb4f8adb6c21a6e1b332feb
Wei En <wei2912.supp0rt@gmail.com>
2014-12-14 17:13:43 +0800

    Use consistent indentation of 4 spaces.

--- example.py
+++ example.py
@@ -2,41 +2,41 @@ import pyrc
 import pyrc.utils.hooks as hooks
 
 class GangstaBot(pyrc.Bot):
-  @hooks.command()
-  def info(self, target, sender):
-    "will print the target and sender to the console"
-    print("target: %s, sender: %s" % (target, sender))
+    @hooks.command()
+    def info(self, target, sender):
+        "will print the target and sender to the console"
+        print("target: %s, sender: %s" % (target, sender))
 
-  @hooks.command()
-  def bling(self, target, sender):
-    "will print yo"
-    if target.startswith("#"):
-      self.message(target, "%s: yo" % sender)
-    else:
-      self.message(sender, "yo")
+    @hooks.command()
+    def bling(self, target, sender):
+        "will print yo"
+        if target.startswith("#"):
+            self.message(target, "%s: yo" % sender)
+        else:
+            self.message(sender, "yo")
 
-  @hooks.command("^repeat\s+(?P<msg>.+)$")
-  def repeat(self, target, sender, **kwargs):
-    "will repeat whatever yo say"
-    if target.startswith("#"):
-      self.message(target, kwargs["msg"])
-    else:
-      self.message(sender, kwargs["msg"])
+    @hooks.command("^repeat\s+(?P<msg>.+)$")
+    def repeat(self, target, sender, **kwargs):
+        "will repeat whatever yo say"
+        if target.startswith("#"):
+            self.message(target, kwargs["msg"])
+        else:
+            self.message(sender, kwargs["msg"])
 
-  @hooks.privmsg("(lol|lmao|rofl(?:mao)?)")
-  def stopword(self, target, sender, *args):
-    """
-    will repeat 'lol', 'lmao, 'rofl' or 'roflmao' when seen in a message
-    only applies to channel messages
-    """
-    if target.startswith("#"):
-      self.message(target, args[0])
+    @hooks.privmsg("(lol|lmao|rofl(?:mao)?)")
+    def stopword(self, target, sender, *args):
+        """
+        will repeat 'lol', 'lmao, 'rofl' or 'roflmao' when seen in a message
+        only applies to channel messages
+        """
+        if target.startswith("#"):
+            self.message(target, args[0])
 
-  @hooks.interval(10000)
-  def keeprepeating(self):
-    "will say something"
-    self.message("#turntechgodhead", "stop repeating myself")
+    @hooks.interval(10000)
+    def keeprepeating(self):
+        "will say something"
+        self.message("#turntechgodhead", "stop repeating myself")
 
 if __name__ == '__main__':
-  bot = GangstaBot('irc.freenode.net', channels = ['#turntechgodhead'])
-  bot.connect()
+    bot = GangstaBot('irc.freenode.net', channels = ['#turntechgodhead'])
+    bot.connect()
--- pyrc/utils/hooks.py
+++ pyrc/utils/hooks.py
@@ -2,47 +2,47 @@ import functools
 import re
 
 class command(object):
-  def __init__(self, matcher=None):
-    self._matcher = matcher
+    def __init__(self, matcher=None):
+        self._matcher = matcher
 
-  def __call__(self, func):
-    # Default the command's name to an exact match of the function's name.
-    # ^func_name$
-    matcher = self._matcher
-    if matcher is None:
-      matcher = r'^%s$' % func.func_name
+    def __call__(self, func):
+        # Default the command's name to an exact match of the function's name.
+        # ^func_name$
+        matcher = self._matcher
+        if matcher is None:
+            matcher = r'^%s$' % func.func_name
 
-    # convert matcher to regular expression
-    matcher = re.compile(matcher)
+        # convert matcher to regular expression
+        matcher = re.compile(matcher)
 
-    @functools.wraps(func)
-    def wrapped_command(*args, **kwargs):
-      return func(*args, **kwargs)
-    wrapped_command._type = "COMMAND"
-    wrapped_command._matcher = matcher
-    return wrapped_command
+        @functools.wraps(func)
+        def wrapped_command(*args, **kwargs):
+            return func(*args, **kwargs)
+        wrapped_command._type = "COMMAND"
+        wrapped_command._matcher = matcher
+        return wrapped_command
 
 class privmsg(object):
-  def __init__(self, matcher=None):
-    self._matcher = matcher
+    def __init__(self, matcher=None):
+        self._matcher = matcher
 
-  def __call__(self, func):
-    # convert matcher to regular expression
-    matcher = re.compile(self._matcher)
+    def __call__(self, func):
+        # convert matcher to regular expression
+        matcher = re.compile(self._matcher)
 
-    @functools.wraps(func)
-    def wrapped_command(*args, **kwargs):
-      return func(*args, **kwargs)
-    wrapped_command._type = "PRIVMSG"
-    wrapped_command._matcher = matcher
-    return wrapped_command
+        @functools.wraps(func)
+        def wrapped_command(*args, **kwargs):
+            return func(*args, **kwargs)
+        wrapped_command._type = "PRIVMSG"
+        wrapped_command._matcher = matcher
+        return wrapped_command
 
 def interval(milliseconds):
-  def wrapped(func):
-    @functools.wraps(func)
-    def wrapped_command(*args, **kwargs):
-      return func(*args, **kwargs)
-    wrapped_command._type = "REPEAT"
-    wrapped_command._interval = milliseconds
-    return wrapped_command
-  return wrapped
+    def wrapped(func):
+        @functools.wraps(func)
+        def wrapped_command(*args, **kwargs):
+            return func(*args, **kwargs)
+        wrapped_command._type = "REPEAT"
+        wrapped_command._interval = milliseconds
+        return wrapped_command
+    return wrapped
